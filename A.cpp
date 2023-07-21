fd2149ffb3230248fdce91bd0c33deed10a12308
AzizullahDarmani <128895230+AzizullahDarmani@users.noreply.github.com>
2023-07-21 15:41:38 +0600

    Add files via upload

---
+++ A.cpp
@@ -0,0 +1,33 @@
+//#include<iostream>
+//using namespace std; 
+//int main() { 
+//int x,y;  
+//cin>> x; 
+//cin>> y;
+//int sum= x+y; 
+//cout<< sum;  
+//
+//
+//}
+#include<iostream>
+#include<cmath> 
+using namespace std;
+int main() {
+//	int x,y; 
+//	cout<< "this is your result: "; 
+//	cin>> x>>y; 
+//	int sum=x+y;
+//	cout<< sum; 
+
+//	int x,y; 
+//	cout<< "this is your result: "; 
+//	cin>> x >> y; 
+//	int sum= x+y; 
+//	cout<< sum; 
+
+
+	int a; 
+	cin>> a; 
+	cout<< "this is the result: " << sqrt(a)<< endl; 
+	
+}
