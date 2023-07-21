f9342871dd9de5cb188313e1be7c00e2e2641b50
Azizullah Darmani <128895230+AzizullahDarmani@users.noreply.github.com>
2023-07-21 16:22:45 +0600

    Add files via upload

---
+++ B.cpp
@@ -0,0 +1,50 @@
+#include<iostream>
+using namespace std; 
+int main() { 
+//int age, digit; 
+//cin>> age; 
+//cin>> digit ; 
+//int first_digit= digit%10; 
+//int last_digit= digit/100; 
+//int result= age+first_digit+ last_digit ;
+//cout<<result;
+
+//	int age, digit; 
+//	cin>> age>> digit ;
+//	int first=digit%10; 
+//	int last= digit/100; 
+//	int done= first+last+age; 
+//	cout<< "this is the final result in our program: " << done; 	
+//	
+//	int Num, integer; 
+//	cin>> Num>> integer; 
+//	int first= integer%10; 
+//	int second= integer /10000; 
+//	int last= integer/100%10; 
+//	int done= Num+first+second+last; 
+//	cout<< "this is the result of whole Num added to first, middle and last numbers of integer: " << done; 
+	
+	// int age, year;
+	// cout<< "Enter your age: " ;
+	// cin>> age ; 
+	// cout<< "Enter you year:" ; 
+	// cin>> year; 
+	// int first= year/100; 
+	// int second= year %10; 
+	// int done= age+first + second; 
+	// cout<< "This is the final number which can be the code: "<< done; 
+
+
+
+	// int age; 
+	// int number; 
+	// cin>> age; 
+	// cin>> number ;
+	// int first = number%10;
+	// int second = number /100; 
+	// int sum = age + first + second ; 
+	// cout<< "the sume of age and the first number and second is: "<< sum << endl ; 
+	// return 0; 
+
+
+}
